82e5fa60bfc1e8395423c68b3f6832b6e425cc36
Jason Danker <jason.danker@ischool.berkeley.edu>
2016-09-27 21:32:56 -0700

    first pass

--- Assignment/HW4_Soundcloud/_js/main.js
+++ Assignment/HW4_Soundcloud/_js/main.js
@@ -1,11 +1,26 @@
+$(document).ready(
+    $("#search").on("click", function() {
+        var query = $("#query").val();
+        api_return = callAPI(query);
+    })
+);
+
 // Event hander for calling the SoundCloud API using the user's search query
 function callAPI(query) {
 	$.get("https://api.soundcloud.com/tracks?client_id=b3179c0738764e846066975c2571aebb",
 		{'q': query,
 		'limit': '200'},
 		function(data) {
-			// PUT IN YOUR CODE HERE TO PROCESS THE SOUNDCLOUD API'S RESPONSE OBJECT
-			// HINT: CREATE A SEPARATE FUNCTION AND CALL IT HERE
+			for (var i = 0; i < 20; i++) {
+				artist = data[i].user.username;
+				title = data[i].title;
+				picture = data[i].artwork_url
+				if (picture == null) {
+					picture = "_images/missing_image.png";
+				};
+				$("#search-results").append("<li>"+artist+" - "+title+"</li><img src='"+picture+"'>");
+			};
+			console.log(data[11]);
 		},'json'
 	);
 }
@@ -23,5 +38,3 @@ function changeTrack(url) {
       links: url
     });
 }
-
-
